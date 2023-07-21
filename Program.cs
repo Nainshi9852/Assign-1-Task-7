2851d5b476bc2f4882df949012f3fe2d4ce4c7cc
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-07-21 21:55:56 +0530

    Task-7

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+            }
+            else
+            {
+                long factorial = CalculateFactorial(number);
+                Console.WriteLine($"Factorial of {number} is: {factorial}");
+            }
+            Console.ReadKey();
+        }
+
+        static long CalculateFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalculateFactorial(n - 1);
+            }
+        }
+ 
+    }
+}
